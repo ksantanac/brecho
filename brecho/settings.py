281241ad207756ea3c3d06b9b5155f9fcd6a4f0c
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2024-10-29 00:46:01 -0300

    add media url

--- brecho/settings.py
+++ brecho/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -128,3 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR, "static")
 ]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
